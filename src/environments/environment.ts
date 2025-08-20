f0a86c1345c1d3e6c51f2b58e2030bc2a8aa0760
Gustavo Sebold <gustavosebold027@gmail.com>
2025-08-19 22:51:19 -0300

    feat(#14): add data fetching with TanStack Query and Axios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,8 @@ export const environment = {
   localStorageKeys: {
     language: 'i18nextLng',
     theme: 'theme'
+  },
+  urls: {
+    baseUrl: ''
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,42 @@
-import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { StrictMode, useMemo } from 'react'
 
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router'
 
 import { ThemeProvider } from './components/ThemeProvider'
 import './globals.css'
+import axios, { AxiosContext } from './lib/facades/axios.facade'
 import './lib/i18n'
 import { routes } from './routes'
+
+const AxiosProvider = ({ children }: React.PropsWithChildren<unknown>) => {
+  const axiosValue = useMemo(() => axios, [])
+
+  return <AxiosContext.Provider value={axiosValue}>{children}</AxiosContext.Provider>
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5 // 5 minutes
+    }
+  }
+})
 
 const router = createBrowserRouter(routes, {})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <AxiosProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark">
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AxiosProvider>
   </StrictMode>
 )
