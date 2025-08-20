791db912dcb3ea60f23f2049b5984dd592eb0f19
Gustavo Sebold <gustavosebold027@gmail.com>
2025-08-19 21:24:37 -0300

    chore: adjust @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './app')
+      '@': path.resolve(__dirname, './src')
     }
   },
   base: '/'
